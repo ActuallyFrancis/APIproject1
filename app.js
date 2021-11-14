9982686f11956639f5e2b8fb5a529134883d9b5c
ActuallyFrancis <94040248+ActuallyFrancis@users.noreply.github.com>
2021-11-14 15:39:52 +0800

    API Key to be fixed

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const api = {
-    key: "f32763d077dee06e885277079b1bf851",
-    baseurl: ""
+    key: "0eb36101fd90d52de1c9c4ab0f26411a",
+    baseurl: "https://api.openweathermap.org/data/2.5"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt){
+    if (evt.keyCode == 13) { //13 is return key or "enter"
+        getResults(searchbox.value);
+        console.log(searchbox.value)
+    }
+}
+
+function getResults (query) {
+    fetch(`${api.baseurl}weather?q=#${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather){
+    console.log(weather);
 }
